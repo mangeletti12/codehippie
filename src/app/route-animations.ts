79c6f6c96b3be584ed057dd035f7240112e6bb17
MarkAngeletti <Mark.Angeletti@katerra.com>
2020-05-11 11:35:43 -0600

    fader animation fix

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -52,18 +52,39 @@ export const fader =
   trigger('fader', [
     transition('* => *', [
       query(':enter, :leave',
-        style({ position: 'fixed', width: '100%' }),
+        style({
+          position: 'relative',
+          width: '100%'
+        }),
         { optional: true }),
       group([
         query(':enter', [
-          style({ opacity: 0 }),
+          style({
+            position: 'absolute',
+            top: '65px',
+            left: 0,
+            width: '100%',
+            opacity: 0
+          }),
           animate('1.5s ease-in',
-            style({ opacity: 1 }))
+            style({
+              opacity: 1,
+            })
+          )
         ], { optional: true }),
         query(':leave', [
-          style({ opacity: 1 }),
+          style({
+            position: 'absolute',
+            top: '65px',
+            left: 0,
+            width: '100%',
+            opacity: 1,
+          }),
           animate('1s ease-out',
-            style({ opacity: 0 }))
+            style({
+              opacity: 0
+            })
+          )
         ], { optional: true }),
       ]),
     ])
