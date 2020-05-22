acc5ae394c2d54885b93c133d238d4a40043a86b
MarkAngeletti <Mark.Angeletti@katerra.com>
2020-05-22 12:13:46 -0600

    list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -14,6 +14,21 @@ export class ListComponent implements OnInit {
   list2: any[] = [];
   list3: any[] = [];
 
+  characters = [
+    {name: "Mara Jade", side: "light"},
+    {name: "Mace Windu", side: "light"},
+    {name: "Obi-Wan Kenobie", side: "light"},
+    {name: "Yoda", side: "light"},
+    {name: "Kit Fisto", side: "light"},
+    {name: "Ahsoka Tano", side: "light"},
+    {name: "Darth Maul", side: "dark"},
+    {name: "Count Dooku", side: "dark"},
+    {name: "Aurra Sing", side: "dark"},
+    {name: "Sheev Palpatine", side: "dark"},
+    {name: "Revan", side: "dark"},
+    {name: "Malak", side: "dark"},
+  ];
+
   constructor(
     private listService: ListService,
   ) {
@@ -21,9 +36,31 @@ export class ListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getList(1);
-    this.getList(2);
-    this.getList(3);
+    this.list1 = this.shuffle(this.characters);
+
+    // this.getList(1);
+    // this.getList(2);
+    // this.getList(3);
+  }
+
+  // Shuffle array
+  shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 
   //Get list
--- src/app/qa/qa.component.ts
+++ src/app/qa/qa.component.ts
@@ -11,6 +11,29 @@ export class QaComponent implements OnInit {
   users: any[] = [];
   qAndA: any[] = [];
 
+  colors = [
+    "#bde052",
+    "#e23dd0",
+    "#86e98f",
+    "#bde052",
+    "#DBF6FA",
+    "#E6B0AA",
+    "#FADBD8",
+    "#EBDEF0",
+    "#a3e4d7",
+    "#aed6f1",
+    "#ebdef0",
+    "#fadbd8",
+    "#f5b7b1",
+    "#d1f2eb",
+    "#a9dfbf",
+    "#a3e4d7",
+    "#aed6f1",
+    "#ebdef0",
+    "#fadbd8",
+    "#f5b7b1",
+]
+
   constructor(
     private qaService: QaService,
   ) { }
@@ -40,11 +63,55 @@ export class QaComponent implements OnInit {
     this.qaService.getUsersAnswers(me).subscribe(
       data => {
         console.log('getQuestionsByUser', data);
+
+        // set a random color
+        for (let i = 0; i < data.length; i++) {
+          const color = this.setRandomColor();
+          data[i].data['color'] = color;
+          data[i].data['lightColor'] = this.LightenDarkenColor(color, -40);
+        }
+
+        // Attach a random color
         this.qAndA = data;
       }
     );
 
   }
+
+  // Get random color from array
+  setRandomColor() {
+    return this.colors[Math.floor(Math.random() * this.colors.length)]
+  }
+
+  // Lighten or Darken color
+  LightenDarkenColor(col, amt) {
+
+    var usePound = false;
+
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
 
 
 }
