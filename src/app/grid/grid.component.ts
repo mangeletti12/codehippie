96e770aec5701098e46802bbed03af29de9b6b6b
Mark Angeletti <markangeletti@Marks-Air.domain>
2020-07-07 14:48:36 -0600

    grid

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 
+interface Tile {
+  row: string;
+  column: string;
+  bot: string;
+  botDirection: number;
+}
+
+class TileMaker {
+  static create(row: string, column: string) {
+    return { row: row, column: column, bot: null, botDirection: null };
+  }
+}
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -13,33 +26,34 @@ export class GridComponent implements OnInit {
   gridRows;
   cells: any[] = [];
   square = 20;
-  //
-  startSquareValue;
+  floorGrid = [];
+  isFloorSet = false;
+  isBotSet = false;
+  tile;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.test();
+    // this.test();
+    
     this.setGrid();
   }
 
   // Listen for window resize
-  @HostListener('window:resize', ['$event'])
-  onResize(e) {
-    const vp = e.target.innerWidth;
-    this.setGrid();
-  }
+  // @HostListener('window:resize', ['$event'])
+  // onResize(e) {
+  //   const vp = e.target.innerWidth;
+  //   this.setGrid();
+  // }
 
   setGrid() {
     // Set carousel width and slides displayed count
     this.floorWidth = (this.floor.nativeElement as HTMLElement).offsetWidth;
     this.floorHeight = (this.floor.nativeElement as HTMLElement).offsetHeight;
-    // this.numberOfSlidesDisplayed = Math.floor(this.carouselWidth/this.slideItemWidth) - 1; //-1 for btn spacing
-    // this.slideHolderWidth = (this.numberOfSlidesDisplayed*this.slideItemWidth);
-    console.log(this.floorWidth + ' - ' + this.floorHeight);
-    //this.calcGrid();
+    // console.log(this.floorWidth + ' - ' + this.floorHeight);
   }
 
+  //
   calcGrid(value) {
     this.cells = [];
     this.square = value;
@@ -47,29 +61,121 @@ export class GridComponent implements OnInit {
     this.gridColumns = Math.floor(this.floorWidth/value);
     this.gridRows = Math.floor(this.floorHeight/value);
 
+    // console.log(this.gridColumns + ' - ' + this.gridRows);
     for(let row = 1; row <= this.gridRows; row++) {
       //
       for(let column = 1; column <= this.gridColumns; column++) {
-        this.cells.push({place: row +':'+ column});
+        this.cells.push(row +':'+ column);
       }
-
     }
 
-    console.log('cells', this.cells);
-
+    // console.log('cells', this.cells);
   }
 
-  //
-  onInputChange(e) {
-    console.log('slide', e.value);
+  // slider change
+  onSliderChange(e) {
+    // console.log('slide', e.value);
     this.calcGrid(e.value);
   }
 
-  //
+  // Take the cells and create rows
+  gridArrayRows(arr) {
+    //
+    let newArr = [];
+    let preRow = 0;
+    let preCcolumn = 0;
+    let localArr = [];
+
+    arr.forEach((data) => {
+
+      let array = data.split(':');
+      let row = array[0];
+      let column = array[1];
+      
+      // create tile
+      const tile = TileMaker.create(row, column);
+
+      if (preRow === +row) {
+        localArr.push(tile);
+      } else {
+        if (localArr.length === 0) {
+          localArr.push(tile);
+        } else {
+          newArr.push(localArr);
+          localArr = [];
+          localArr.push(tile);
+        }
+      }
+
+      preRow = +row;
+      preCcolumn = +column;
+    });
+    //
+    newArr.push(localArr);
+    console.log(newArr);
+    return newArr;
+  }
+
+  // Set grid button
+  createGrid() {
+    this.floorGrid = this.gridArrayRows(this.cells);
+    this.isFloorSet = true;
+    // console.log('createGrid', this.floorGrid);
+  }
+
+  // Select a square from floor
   squareSelect(item) {
+    // only one
+    this.floorGrid.forEach(row => {
+      row.forEach(element => {
+        element.selected = false;
+      });
+    });
+
+    item.selected = !item.selected;
+    this.tile = item;
+
     console.log('squareSelect', item);
   }
 
+  //
+  setBot() {
+    // console.log('tile', this.floorGrid);
+
+    this.floorGrid.forEach(row => {
+
+      row.forEach(element => {
+        
+        if(element.row === this.tile.row && element.column === this.tile.column) {
+          //
+          this.tile.bot = 'r2';
+          this.tile.botDirection = 1; 
+          console.log('setBot', this.tile);
+    
+        }
+
+      });
+
+    });
+
+    
+  }
+
+  // Turn bot
+  turnBot() {
+    if (this.tile.botDirection !== 4) {
+      this.tile.botDirection = this.tile.botDirection +1; 
+    } else {
+      this.tile.botDirection = 1;
+    }
+
+    console.log('turnBot', this.tile);
+  }
+
+
+
+
+  // Testing
   test() {
     let array1 = [1,2,3];
     let cArray = [...array1]; // copy array
@@ -80,8 +186,9 @@ export class GridComponent implements OnInit {
 
     const addMoreArray = [...array1, 5,6];
     console.log('addMoreArray', addMoreArray); // [1, 2, 3, 4, 5, 6]
-
   }
+
+
 
 
 }
